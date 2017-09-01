01c8c0c553354f13d211e65fdc13cee99c2da04a
CS Ahn <information@feyn.co>
2017-09-01 12:52:23 -0700

    d3.js map ready for Census API input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,24 @@
-
 var svg = d3.select("svg");
 
 var path = d3.geoPath();
 
 d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-  if (error) throw error;
+    if (error) throw error;
 
-  svg.append("g")
-    .attr("class", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-    .attr("d", path);
+    svg.append("g")
+        .attr("class", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        .attr("d", path);
 
-  svg.append("path")
-      .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+    svg.append("path")
+        .attr("class", "state-borders")
+        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 
- });
-  
+});
+
+//Heatmap generator
+//Uses d3.js
+//Parameter: 50-element text 
+//Parameters: 50-element integer or float array indicating population count for each state
