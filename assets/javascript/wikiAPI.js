483849797e544e6e2bfdb5b1e9acecb67f20e038
Richard Selznick <richardselz@gmail.com>
2017-09-01 00:39:10 -0700

    Working Wiki API

--- assets/javascript/wikiAPI.js
+++ assets/javascript/wikiAPI.js
@@ -1,5 +1,8 @@
-var queryGET = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exchars=1000&titles=Spanish_language";
+var queryGET = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=Spanish_language";
 // var queryGET = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exchars=1000&titles="+currentLanguage+"Spanish_language";
+//https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch= 
+//https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exchars=1000&titles=Spanish_language
+
 
 $.ajax({
 		url: queryGET,
